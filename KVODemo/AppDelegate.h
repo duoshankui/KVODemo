e37ac45b271a8df11f74f70571ce9a5b3a48fbdc
DoubleK <2578138035@qq.com>
2018-03-07 09:50:43 +0800

    Initial Commit

---
+++ KVODemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVODemo
+//
+//  Created by DoubleK on 2018/3/7.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVODemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVODemo
+//
+//  Created by DoubleK on 2018/3/7.
+//  Copyright © 2018年 DoubleK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
